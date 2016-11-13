eff500d2d9098e4c90809f56ff4204773cda40b2
vdemedes <vdemedes@gmail.com>
2016-11-13 18:07:09 +0200

    simplify code, thanks @sindresorhus!

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const repeating = require('repeating');
-
 module.exports = (str, spaces) => {
-	spaces = spaces || 2;
-
-	return str.replace(/^\t+/gm, $1 => repeating($1.match(/\t/g).length * spaces));
+	return str.replace(/^\t+/gm, $1 => ' '.repeat($1.length * (spaces || 2)));
 };
